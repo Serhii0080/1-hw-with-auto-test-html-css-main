3394e27324cdb44c6ab7a2ef95b81f445f7d0a64
Serhii0080 <startap.chrom@gmail.com>
2025-11-24 23:13:11 +0200

    lesson3-task3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,13 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number1, number2, number3) {
+    if (number1 > number2 && number3) {
+        return number1;
+    } else if (number2 > number1 && number3) {
+        return number2;
+    } else if (number3 > number1 && number2) {
+        return number3;
+    }
+}
+
+findLargest(355, 12, 172);
