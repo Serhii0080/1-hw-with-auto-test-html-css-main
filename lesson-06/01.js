8d95d7ad7c84edda3bde5b544d90fc6372cdae65
Serhii0080 <startap.chrom@gmail.com>
2025-12-17 15:06:26 +0200

    lesson6-task1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -16,11 +16,26 @@
 /* Пример использования:
 const numbers = [1, 2, 3, 4, 5]
 
-const oddNumbers = filter(numbers, (element, index) => {
-  return element % 2 !== 0
-});
+const oddNumbers = filter(numbers, (element, index) => {return element % 2 !== 0});
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const numbers = [1, 2, 3, 4, 5];
+
+const filter = (array, callback) => {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i)) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+};
+
+const oddNumbers = filter(numbers, (element, index) => {
+    return element % 2 !== 0;
+});
+
+console.log(oddNumbers);
