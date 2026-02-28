f7f8e48402b931dfbec205ddd85567a21f3300d1
Serhii0080 <startap.chrom@gmail.com>
2026-02-28 10:56:46 +0200

    remove unused files

--- lesson-03/01.js
+++
@@ -1,8 +0,0 @@
-// Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
-
-function calculateAverage(number1, number2) {
-    const sum = (number1 + number2) / 2;
-    return sum;
-}
-
-console.log(calculateAverage(50, 100));
